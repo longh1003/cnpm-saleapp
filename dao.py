570338a311cc229e0a29177bcdeb18d8f40bf22f
unknown <longmap2003@gmail.com>
2025-10-22 10:54:34 +0700

    error

--- dao.py
+++ dao.py
@@ -16,5 +16,14 @@ def load_products(q=None, id=None):
             products = [ p for p in products if p["cate_id"].__eq__(int(id))]
         return products
 
+def load_products_by_id(id):
+    with open("data/products.json", encoding="utf-8") as f:
+        products = json.load(f)
+    if id:
+        products = [ p for p in products if p["id"].__eq__(int(id))]
+        print()
+    # fix this
+    return products
+
 if __name__=="__main__":
     pass
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+import dao
 from dao import load_categories, load_products
 
 app = Flask(__name__)
@@ -13,6 +14,11 @@ def index():
     prods = load_products(q, cate_id)
     return render_template("index.html", name=name, cates=cates, prods=prods)
 
+
+@app.route("/products/<int:id>")
+def details(id):
+    product = dao.load_products_by_id(id)
+    return render_template("product-details.html", product=product)
 
 if __name__=="__main__":
     with app.app_context():
