c82976fca20ff54d6817c936b8915b673ef9abc9
unknown <longmap2003@gmail.com>
2025-11-19 09:10:25 +0700

    add User model

--- saleapp/models.py
+++ saleapp/models.py
@@ -1,22 +1,41 @@
 import json
 
 from saleapp import app,db
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Boolean, DateTime, Enum
 from sqlalchemy.orm import relationship
+from datetime import datetime
+from enum import Enum as EnumRole
 
-
-class Category(db.Model):
-    id = Column(Integer, autoincrement=True, primary_key=True)
+class Base(db.Model):
+    __abstract__=True
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(150), nullable=False, unique=True)
+    created_date = Column(DateTime, default=datetime.now())
+    active = Column(Boolean, default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(Base):
     products = relationship('Product', backref='category', lazy=True)
 
+class UserRole(EnumRole):
+    USER = 1
+    ADMIN = 2
 
-class Product(db.Model):
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String(150), nullable=False, unique=True)
+class Product(Base):
     image = Column(String(300), nullable=False, default="https://res.cloudinary.com/dy1unykph/image/upload/v1729091967/419VTYVRD1L._AC__vih8qs.jpg")
     price = Column(Float, default=0.0)
     cate_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+class User(Base):
+    username = Column(String(20), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    avatar = Column(String(300), default="https://res.cloudinary.com/dy1unykph/image/upload/v1729091967/419VTYVRD1L._AC__vih8qs.jpg")
+    role = Column(Enum(UserRole), default=UserRole.USER)
+
 
 if __name__=="__main__":
     with app.app_context():
